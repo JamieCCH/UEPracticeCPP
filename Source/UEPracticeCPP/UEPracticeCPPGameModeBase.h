33a7c150093360324b06da4ee63088f49234f7af
Jamie C Huang <jamie.cch@gmail.com>
2023-06-22 18:05:06 -0400

    Create project

---
+++ Source/UEPracticeCPP/UEPracticeCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UEPracticeCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEPRACTICECPP_API AUEPracticeCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
